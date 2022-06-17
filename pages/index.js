288fdcfde6ccc4c6d4ab1818d3881d0c39e4228d
Wil Hesk <wilhesk@gmail.com>
2022-06-17 15:59:10 +0100

    stylingedit2

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <div>
        
         <h1 className={styles.title}>WELCOME TO HESK DESIGN</h1>
-        <h2>HELPING CREATE EFFICIENT, WELL DESIGNED CONTENT THAT SELLS YOUR PRODUCT</h2>
+        <h2 className={styles.title}>HELPING CREATE EFFICIENT, WELL DESIGNED CONTENT THAT SELLS YOUR PRODUCT</h2>
         <p className={styles.text}>We know as a small business owner or sole trader the jobs are neverending! We can help you develop an honest and engaging brand story that connects with your customers emotionally - leading to improved sales</p>
         <p className={styles.text}>Our website is currently under construction, please join our mailing list if you wish to take advantage of discount offers we will be releasing upon completeion of the site.</p>
   
