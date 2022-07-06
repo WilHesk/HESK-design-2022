dbe3b732a69ab959c8991ff3839d7231020e587b
Wil Hesk <wilhesk@gmail.com>
2022-07-06 13:16:16 +0100

    textedit

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ export default function Home() {
     <>
       <Head>
         <title>Hesk Design | Home</title>
-        <meta name="keywords" content="hesk design manchester brand and marketing design"/>
+        <meta name="keywords" content="hesk design manchester ethical brand and marketing design"/>
       </Head>
       <div>
        
         <h1 className={styles.title}>WELCOME TO HESK DESIGN</h1>
-        <h2 className={styles.title}>HELPING CREATE EFFICIENT, WELL DESIGNED CONTENT THAT SELLS YOUR PRODUCT</h2>
+        <h2 className={styles.title}>I DESIGN EFFICIENT MARKETING FOR ORGANISATIONS THAT WANT TO MAKE THE WORLD A BETTER PLACE - SO WE CAN MAKE IT BETTER FASTER!</h2>
         <p className={styles.text}>We know as a small business owner or sole trader the jobs are neverending and the pressure to be putting out marketing content is high! We can help you develop an honest and engaging brand story that connects with your customers emotionally - leading to improved sales.</p>
         <p className={styles.text}>Our website is currently under construction!!!!</p>
 
